bf79724ae92596895ea92df2aaa005cde5af9eb7
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-05-25 10:56:39 +0530

    jskart page commit

---
+++ jskart/jskart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jskart',
+  standalone: true,
+  imports: [],
+  templateUrl: './jskart.component.html',
+  styleUrl: './jskart.component.css'
+})
+export class JskartComponent {
+
+}
